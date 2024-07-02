ba2491a42a46e1a92efae147db76fc68b03a7774
Jefferson Cheng <123587694+cimmanonroll@users.noreply.github.com>
2024-07-02 15:49:51 -0400

    Update Automation.py

--- Automation.py
+++ Automation.py
@@ -51,7 +51,7 @@ def generate_job_description(text):
     {text}
 
     """
-    url = "http://10.1.0.133:11434/api/chat"  # URL of the Ollama server
+    url = "http://10.1.0.133:11434/api/generate"  # URL of the Ollama server
 
     try:
         response = requests.post(url, json={"prompt": prompt})
