fe1a6ed847d9aa7dc93586c643f2e6e6410272a8
Jefferson Cheng <123587694+cimmanonroll@users.noreply.github.com>
2024-07-08 13:36:51 -0400

    Update Automation.py

--- Automation.py
+++ Automation.py
@@ -1,99 +1,117 @@
 import streamlit as st
-import fitz  # PyMuPDF
+import fitz  # PyMuPDF
 import spacy
-import requests
 from langchain_community.llms import Ollama
 import tempfile
-
-
+import re
+ 
 nlp = spacy.load("en_core_web_sm")
-
+ 
 llm = Ollama(model="llama3")
-
-# Gets the job title from the text
+ 
 def get_job_title(text):
-    doc = nlp(text)
-    job_title = []
-    capture = False
-    for sent in doc.sents:
-        if "title" in sent.text.lower():
-            capture = True
-       
-        if capture:
-            job_title = sent.text.strip()
-            break
-    job_title = job_title.split("\n")
-    index = job_title.index("Task Order Title: ")
-    return job_title[index + 1]
-
+    doc = nlp(text)
+    job_title = []
+    capture = False
+    for sent in doc.sents:
+        if "title" in sent.text.lower():
+            capture = True
+        if capture:
+            job_title = sent.text.strip()
+            break
+    job_title = job_title.split("\n")
+    index = job_title.index("Task Order Title: ")
+    return job_title[index + 1]
+ 
 def extract_text_from_pdf(file_path):
-    pdf_document = fitz.open(file_path)
-    text = ""
-    for page_num in range(pdf_document.page_count):
-        page = pdf_document.load_page(page_num)
-        text += page.get_text()
-    return text
-
-#changes NEEDED
-def generate_job_description(text): 
-    prompt = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\nYou are hiring recruiter at Technuf LLC who specializes in creating a clear and concise job description based on a solicitations document<|eot_id|>"
-    prompt += "<|start_header_id|>user<|end_header_id|>\nBased on the document given, generate a fully filled out job requirements document that follows the following template exactly. Do not include anything that is not required by the template.\n"
-    prompt += "What were looking for\n [Insert a one paragraph description of the name of the job that Technuf is looking for and what the job entails (make sure to start the paragraph with: Technuf is looking for...)]\n"
-    prompt += "Responsibilities: \n [Insert responsibilities from the scope of work section here]\n"
-    prompt += "Skills and Experience: \n [Insert needed skills and years of experience from the experience section here]\n"
-    prompt += "Good to Have: \n [Insert skills from the \"Additionally, to be successful\" section here]\n"
-    prompt += "Education: \n "
-    prompt += "Required: [insert required level of education here] \n "
-    prompt += "Preferred: [insert preferred level of education here]\n"
-    prompt += f"""
-    Generate a job posting for Technuf LLC based in Maryland, USA. Use the information from the following text to create the job description using the template above:
-
-    {text}
-
-    """
-    url = "http://10.1.0.133:11434/api/generate"  # URL of the Ollama server
-
-    try:
-        response = requests.post(url, json={"prompt": prompt})
-        response.raise_for_status()  # Check for HTTP errors
-        return response.json()["generated_text"]
-    except requests.exceptions.RequestException as e:
-        st.error(f"Error generating job description: {e}")
-        return None
-#changes NEEDED
-
+    pdf_document = fitz.open(file_path)
+    text = ""
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_num)
+        text += page.get_text()
+    return text
+ 
+def extract_relevant_sections(text):
+    pattern = re.compile(r'^(.*?)(Scope of Work.*?)(Skills/Experience.*?)(Additionally, to be successful.*?)(?=\n[A-Z]|$)', re.DOTALL | re.IGNORECASE)
+    match = pattern.search(text)
+    if match:
+        before_scope_of_work = match.group(1).strip()
+        scope_of_work = match.group(2).strip()
+        skills_experience = match.group(3).strip()
+        additionally_successful = match.group(4).strip()
+        return before_scope_of_work + "\n\n" + scope_of_work + "\n\n" + skills_experience + "\n\n" + additionally_successful
+    return ""
+ 
+ 
+def generate_job_description(text):
+    prompt = f"""
+    Generate a job posting for Technuf LLC based in Maryland, USA. Use the information from the following text to create the job description using the template below:
+   
+    What we’re looking for [Do not change this header]:
+    [Insert a one paragraph description of the name of the job that Technuf is looking for and what the job entails (make sure to start the paragraph with: Technuf is looking for...)]
+   
+    Responsibilities [Do not change this header]:
+    [This section should copy everything from the Scope of Work section, in bullet points]
+   
+    Skills and Experience [Do not change this header]:
+    [This section should copy everything from the Skills/Experience or Minimum Qualifications, Education and Experience section, in bullet points]
+   
+    Good to Have [Do not change this header]:
+    [This section should copy everything from the Additionally, to be successful section]
+   
+    Education [Do not change this header]:
+    Required: [insert required level of education here]
+    Preferred: [insert preferred level of education here]
+   
+    Benefits [Do not change this header]:
+    We offer a competitive pay and benefits package that includes generous paid-time-off including holidays, short-and-long-term disability; group health insurance including medical, dental and vision coverage, training and 401(k) retirement plan.
+   
+    Technuf is an Equal Opportunity/Affirmative Action Employer. Members of ethnic minorities, women, special disabled veterans, veterans of the Vietnam-era, recently separated veterans, and other protected veterans, persons of disability and/or persons age 40 and over are encouraged to apply.
+   
+    Ensure to exclude any mention of Evaluation Criteria, How to Apply, Note, and Interview Criteria.
+ 
+    Use the information from the text below to fill in the relevant sections:
+   
+    {text}
+    """
+   
+    response = llm.invoke(prompt)
+    return response
+ 
+# IMPORTANT
+def clean_generated_text(text):
+    cleaned_text = "\n".join([line for line in text.split("\n") if not line.strip().startswith("Here is")])
+    return cleaned_text
+ 
 def main():
-    st.title("Technuf Automation")
-    st.write("Upload a PDF file")
-
-    uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
-
-    if uploaded_file is not None:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
-            temp_file.write(uploaded_file.read())
-            temp_filepath = temp_file.name
-
-        pdf_text = extract_text_from_pdf(temp_filepath)
-
-        if pdf_text:
-            st.subheader("Extracted Text")
-            st.write(pdf_text)
-            if st.button("Generate Job Description"):
-                with st.spinner('Generating job posting'):
-                    # job_posting = get_job_title(pdf_text) + "\n\n" + "Who we are \n Technuf, LLC is a Maryland based SBA certified 8(a) small business company providing leading-edge and proven technologies, industry vertical domain expertise and highly skilled and motivated professionals to achieve our customers mission critical business needs.\n\n"
-                    job_description = generate_job_description(pdf_text)
-                    # index_of_newline = job_description.rfind('\n')
-                    st.subheader("Generated Job Description")
-                    
-                    #if "Preferred:" not in job_description[index_of_newline:]:
-                    #    job_description = job_description[:index_of_newline]
-                    #job_posting += job_description
-                    #job_posting += "\n\nBenefits:\nWe offer a competitive pay and benefits package that includes generous paid-time-off including holidays, short-and-long-term disability; group health insurance including medical, dental and vision coverage, training and 401(k) retirement plan. \n"
-                    #job_posting += "\nTechnuf is an Equal Opportunity/Affirmative Action Employer. Members of ethnic minorities, women, special disabled veterans, veterans of the Vietnam-era, recently separated veterans, and other protected veterans, persons of disability and/or persons age 40 and over are encouraged to apply.\n"
-                    #st.write(job_posting)
-                    st.write(job_description)
-        else:
-            st.error("No text found in the uploaded PDF.")
-
+    st.title("Technuf Automation")
+    st.write("Upload a PDF file")
+ 
+    uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+ 
+    if uploaded_file is not None:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+            temp_file.write(uploaded_file.read())
+            temp_filepath = temp_file.name
+ 
+        pdf_text = extract_text_from_pdf(temp_filepath)
+        relevant_sections = extract_relevant_sections(pdf_text)
+ 
+        if relevant_sections:
+            st.subheader("Extracted Relevant Sections")
+            st.write(relevant_sections)
+            if st.button("Generate Job Description"):
+                with st.spinner('Generating job posting'):
+                    job_posting = get_job_title(relevant_sections) + "\n\n" + "Who we are \n Technuf, LLC is a Maryland based SBA certified 8(a) small business company providing leading-edge and proven technologies, industry vertical domain expertise and highly skilled and motivated professionals to achieve our customers mission critical business needs.\n\n"
+                    job_description = generate_job_description(relevant_sections)
+                    cleaned_job_description = clean_generated_text(job_description)
+                    job_posting += cleaned_job_description
+                    job_posting += "\n\nBenefits:\nWe offer a competitive pay and benefits package that includes generous paid-time-off including holidays, short-and-long-term disability; group health insurance including medical, dental and vision coverage, training and 401(k) retirement plan. \n"
+                    job_posting += "\nTechnuf is an Equal Opportunity/Affirmative Action Employer. Members of ethnic minorities, women, special disabled veterans, veterans of the Vietnam-era, recently separated veterans, and other protected veterans, persons of disability and/or persons age 40 and over are encouraged to apply.\n"
+                    st.subheader("Generated Job Description")
+                    st.write(job_posting)
+        else:
+            st.error("No relevant sections found in the uploaded PDF.")
+ 
 if __name__ == "__main__":
-    main()
+    main()
